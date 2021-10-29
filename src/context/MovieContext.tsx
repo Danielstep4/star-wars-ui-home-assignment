05ddfaeff473155c6c4cef566b0ac21e97274434
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-10-29 20:06:30 +0300

    Added more funcs to context Added two funcs to handle favorite state Added func to change current movie which will display on movie details comp Added func to get all movies names to display in sidebar

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -1,4 +1,5 @@
 import { useContext, createContext, useState, useEffect } from "react";
+import { useToggler } from "../hooks/useToggler";
 
 // Init Context
 ///@ts-ignore
@@ -12,7 +13,9 @@ const MoviesContextProvider: React.FC<MoviesProviderProps> = ({
   data,
 }) => {
   // State
-  const [favoriteMovies, setFavoriteMovies] = useState<SwapiMoviesResult[]>([]);
+  const [favoriteMovies, setFavoriteMovies] = useState<FavoriteMovie[]>([]);
+  const [updateLocalStorage, toggleUpdateLocalStorage] = useToggler(false);
+  const [currentMovie, setCurrentMovie] = useState<SwapiMoviesResult>(data[0]);
   // On load getting all favorite movies from local storage
   useEffect(() => {
     const data = localStorage.getItem("favoriteMovies");
@@ -21,25 +24,60 @@ const MoviesContextProvider: React.FC<MoviesProviderProps> = ({
       if (Array.isArray(favoriteMovies)) setFavoriteMovies(favoriteMovies);
     }
   }, []);
+  /** Every time the user updates favorite Movies state the use effect will save it in localStorage */
+  useEffect(() => {
+    if (updateLocalStorage) {
+      localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+      toggleUpdateLocalStorage();
+    }
+  }, [updateLocalStorage]);
   // Helper functions
-  const getAllMoviesNames = () => {
-    return data.map((movie) => movie.title);
+  /** Returns one movie by episode id or undefined */
+  const _findOneMovieByEpisodeId = (episodeId: number) => {
+    return data.find((movie) => movie.episode_id === episodeId);
   };
-  const setNewFavoriteMovie = (movieTitle: string) => {
-    if (!movieTitle) return;
+  /** Returns all movies from data prop sorted by episode id */
+  const getAllMoviesNames = () => {
+    const allMoviesNames: MovieName[] = data.map((movie) => ({
+      title: movie.title,
+      episodeId: movie.episode_id,
+      isFavorite: !!favoriteMovies.find(
+        (favMovie) => favMovie.episodeId === movie.episode_id
+      ),
+    }));
+    allMoviesNames.sort((a, b) => a.episodeId - b.episodeId);
+    return allMoviesNames;
+  };
+  /** Pushes new favorite movie to favoriteMovies state */
+  const setNewFavoriteMovie = (episodeId: number) => {
+    if (!episodeId) return;
     setFavoriteMovies((prev) => {
-      const movie = data.find((movie) => movie.title === movieTitle);
-      if (movie) {
-        prev.push(movie);
-      }
+      prev.push({ episodeId });
       return prev;
     });
-    localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+    toggleUpdateLocalStorage();
+  };
+  /** Filters favoriteMovies array to remove movie  */
+  const removeFavoriteMovie = (episodeId: number) => {
+    if (!episodeId) return;
+    setFavoriteMovies((prev) =>
+      prev.filter((movie) => movie.episodeId !== episodeId)
+    );
+    toggleUpdateLocalStorage();
+  };
+  /** Changes the current movie state to be the selected movie if it exists */
+  const changeCurrentMovie = (episodeId: number) => {
+    if (!episodeId) return;
+    const selectedMovie = _findOneMovieByEpisodeId(episodeId);
+    if (selectedMovie) setCurrentMovie(selectedMovie);
   };
   // Value
   const value: MoviesValue = {
+    currentMovie,
     getAllMoviesNames,
+    changeCurrentMovie,
     setNewFavoriteMovie,
+    removeFavoriteMovie,
   };
   // Provider
   return (
@@ -50,8 +88,11 @@ const MoviesContextProvider: React.FC<MoviesProviderProps> = ({
 export default MoviesContextProvider;
 
 interface MoviesValue {
-  getAllMoviesNames: () => string[];
-  setNewFavoriteMovie: (movieTitle: string) => void;
+  currentMovie: SwapiMoviesResult;
+  getAllMoviesNames: () => MovieName[];
+  changeCurrentMovie: (episodeId: number) => void;
+  setNewFavoriteMovie: (episodeId: number) => void;
+  removeFavoriteMovie: (episodeId: number) => void;
 }
 
 interface MoviesProviderProps {
