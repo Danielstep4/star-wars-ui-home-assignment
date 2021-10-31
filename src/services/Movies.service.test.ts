f778544579e37352abaaf0d52bdee4a7e2960a09
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-10-31 10:20:55 +0200

    Changed name of test file and added more checking

--- src/services/Movies.service.test.ts
+++ src/services/Movies.service.test.ts
@@ -3,6 +3,7 @@ import { getAllMovies } from "./Movies.service";
 test("getAllMovies func has to return an array with data or an empty array", () => {
   return getAllMovies().then((data) => {
     expect(data).toBeTruthy();
-    expect(data).not.toBeEmpty();
+    expect(Array.isArray(data)).toBe(true);
+    expect(data.length).toBeGreaterThanOrEqual(0);
   });
 });
